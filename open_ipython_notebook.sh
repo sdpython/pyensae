4de996cf3c74e9baac89d7fce0c76016645a6dae
azure provisioned user <pyxavierdupre@ubuntu12python.ubuntu12python.a4.internal.cloudapp.net>
2014-11-23 18:29:37 +0000

    add bash script for linux

---
+++ open_ipython_notebook.sh
@@ -0,0 +1,3 @@
+export pythonexe=$HOME/anaconda3/bin/python
+export PYTHONPATH=$PYTHONPATH:../../src:../../../pymyinstall/src:../../../pyquickhelper/src
+ipython notebook --notebook-dir=_doc/notebooks
