5cdf3968c7210cb4e087192a693b885c1ee29f9b
dupre <xavier.dupre@ensae.fr>
2015-03-23 12:09:21 +0100

    update conf.py, remove unnecessary file

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -17,12 +17,6 @@ sys.path.insert(
     os.path.abspath(
         os.path.join(
             os.path.split(__file__)[0],
-            "pyensae")))
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.split(__file__)[0],
             "..",
             "..",
             "..",
