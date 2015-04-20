d92ba82d23b4addfd08e7181426d7dcb63dce91c
dupre <xavier.dupre@ensae.fr>
2015-04-20 23:43:40 +0200

    raise an exception when version.txt is not found

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ if os.path.exists("version.txt"):
         lines = f.readlines()
     subversion = lines[0].strip("\r\n ")
 else:
-    subversion = 1
+    raise FileNotFoundError("version.txt")
 
 project_var_name = "pyensae"
 sversion = "1.1"
