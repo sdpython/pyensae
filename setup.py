5c6ca4967d8b778ec92735e7d19d8fd86159b0b1
dupre <xavier.dupre@ensae.fr>
2015-12-27 17:41:09 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -148,12 +148,15 @@ if "--verbose" in sys.argv:
 
 if is_local():
     pyquickhelper = import_pyquickhelper()
+    logging_function = pyquickhelper.fLOG
+    logging_function(OutputPrint=True)
     r = pyquickhelper.process_standard_options_for_setup(
         sys.argv, __file__, project_var_name,
         unittest_modules=["pyquickhelper", "pymyinstall"],
         requirements=["pyquickhelper", "pymyinstall"],
         additional_notebook_path=["pyquickhelper"],
-        coverage_options=dict(omit=["*Parser.py", "*Listener.py", "*Lexer.py"]))
+        coverage_options=dict(omit=["*Parser.py", "*Listener.py", "*Lexer.py"]),
+        fLOG=logging_function)
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
                        "upload_docs", "bdist_wininst"} & set(sys.argv)):
