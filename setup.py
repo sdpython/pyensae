cc3cb3a75e968898c5342c4654abb3cf256aeb67
xavier dupré <xavier.dupre@gmail.com>
2017-06-30 23:30:24 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -205,7 +205,7 @@ if is_local():
         r = True
     if not r and not ({"bdist_msi", "sdist",
                        "bdist_wheel", "publish", "publish_doc", "register",
-                       "upload_docs", "bdist_wininst"} & set(sys.argv)):
+                       "upload_docs", "bdist_wininst", "build_ext"} & set(sys.argv)):
         raise Exception("unable to interpret command line: " + str(sys.argv))
 else:
     r = False
