56ea2b950b176d8a41b8f1f8089fcb92c00585b8
xavier dupré <xavier.dupre@gmail.com>
2017-08-12 20:16:34 +0200

    unit test to check that #10 is not a bug

---
+++ _unittests/ut_datasource/test_resources_bug.py
@@ -0,0 +1,63 @@
+"""
+@brief      test log(time=2s)
+
+You should indicate a time in seconds. The program ``run_unittests.py``
+will sort all test files by increasing time and run them.
+"""
+
+
+import sys
+import os
+import unittest
+
+
+try:
+    import src
+    import pyquickhelper as skip_
+except ImportError:
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..")))
+    if path not in sys.path:
+        sys.path.append(path)
+    path = os.path.normpath(
+        os.path.abspath(
+            os.path.join(
+                os.path.split(__file__)[0],
+                "..",
+                "..",
+                "..",
+                "pyquickhelper",
+                "src")))
+    if path not in sys.path:
+        sys.path.append(path)
+    import src
+    import pyquickhelper as skip_
+
+from pyquickhelper.loghelper import fLOG
+from pyquickhelper.pycode import get_temp_folder
+from src.pyensae.datasource.http_retrieve import download_data
+
+
+class TestResourcesBug(unittest.TestCase):
+
+    def test_damir(self):
+        fLOG(
+            __file__,
+            self._testMethodName,
+            OutputPrint=__name__ == "__main__")
+
+        temp = get_temp_folder(__file__, "temp_damir")
+        res = download_data("A201612_small.csv.gz", whereTo=temp)
+        self.assertEqual(len(res), 1)
+        checks = [os.path.join(temp, _)
+                  for _ in ["A201612_small.csv", "A201612_small.csv.gz"]]
+        self.assertTrue(os.path.exists(checks[0]))
+        self.assertTrue(os.path.exists(checks[1]))
+
+
+if __name__ == "__main__":
+    unittest.main()
