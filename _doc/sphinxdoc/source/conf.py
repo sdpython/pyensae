2c58ff00851aa55d4a14d4eb6b6f2f8cc8884302
xavier dupré <xavier.dupre@gmail.com>
2018-01-02 02:17:33 +0100

    update license

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(
 
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
-set_sphinx_variables(__file__, "pyensae", "Xavier Dupré", 2017,
+set_sphinx_variables(__file__, "pyensae", "Xavier Dupré", 2018,
                      "basicstrap", None, locals(), add_extensions=None,
                      extlinks=dict(issue=('https://github.com/sdpython/pyensae/issues/%s', 'issue')))
 
