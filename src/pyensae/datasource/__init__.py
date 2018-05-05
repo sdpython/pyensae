74b50019f4b1ed09159c422eda13bcd4be348b6a
xavier dupré <xavier.dupre@gmail.com>
2018-05-05 11:58:37 +0200

    remove import on removed files

--- src/pyensae/datasource/__init__.py
+++ src/pyensae/datasource/__init__.py
@@ -3,6 +3,5 @@
 @brief Shortcuts to datasource
 """
 
-from .data_velib import DataVelibCollect
 from .convert import dBase2df, dBase2sqllite
 from .http_retrieve import download_data, DownloadDataException
