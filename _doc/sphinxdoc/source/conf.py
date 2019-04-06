f549e3ec4ff3e5e49b90733b455f1a46dcaa0541
xavier dupré <xavier.dupre@gmail.com>
2019-04-06 14:48:42 +0200

    update documentation theme

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-import sphinxjp.themes.basicstrap
+import sphinx_readable_theme
 from pyquickhelper.helpgen.default_conf import set_sphinx_variables
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 
 set_sphinx_variables(__file__, "pyensae", "Xavier Dupré", 2019,
-                     "basicstrap", None, locals(), add_extensions=None,
+                     "readable", sphinx_readable_theme.get_html_theme_path(),
+                     locals(), add_extensions=None,
                      extlinks=dict(issue=('https://github.com/sdpython/pyensae/issues/%s', 'issue')))
 
 blog_root = "http://www.xavierdupre.fr/app/pyensae/helpsphinx/"
 blog_background = False
 
 nblinks = {"code-r2python": blog_root + "pyensae/languages/rconverter.html"}
-
-extensions.append("sphinxjp.themes.basicstrap")
 
 epkg_dictionary = ({
     "antlr4": 'https://github.com/antlr/antlr4',
