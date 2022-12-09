8bb758be08fb7ceb05dc98b6f2f82b0451f7168e
xavier dupré <xavier.dupre@gmail.com>
2022-12-10 00:47:39 +0100

    Update plot_map_france.py

--- _doc/examples/plot_map_france.py
+++ _doc/examples/plot_map_france.py
@@ -15,16 +15,17 @@ Just a map of France.
 """
 import sys
 from matplotlib import pyplot as plt
-from matplotlib import pyplot as plt
-import cartopy.crs as ccrs
 from pyensae.datasource import load_french_departements
 from pyensae.graphhelper import plot_map_france, plot_map_france_polygon
 
-ax = plot_map_france()
-# This instruction may introduce a segment fault.
-# See https://github.com/SciTools/cartopy/issues/837.
-ax.set_extent([-5., 10., 38., 52.])
-ax.set_title('France')
+if sys.version_info[:2] >= (3, 10):
+    ax = plot_map_france()
+    # This instruction may introduce a segment fault.
+    # See https://github.com/SciTools/cartopy/issues/837.
+    ax.set_extent([-5., 10., 38., 52.])
+    ax.set_title('France')
+else:
+    print("some issues with the installation on older version of python.")
 
 #######################################
 # .. faqref::
@@ -48,14 +49,15 @@ ax.set_title('France')
 # loads the French departments
 df = load_french_departements()
 
-fig = plt.figure(figsize=(7, 7))
-ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
-N = float(df.shape[0])
 if sys.version_info[:2] >= (3, 10):
+    import cartopy.crs as ccrs
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+    N = float(df.shape[0])
     plot_map_france_polygon(
         ax=ax, geometry=df['geometry'],
         colors=[(i / N, 1. - i / N, 1. - i / N) for i in range(df.shape[0])])
+    ax.set_title('France departments')
 else:
     print("some issues with the installation on older version of python.")
-ax.set_title('France departments')
 # plt.show()
