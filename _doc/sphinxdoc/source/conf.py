f6837501150a3ac471284d1017e14b1e54306b45
xavier dupré <xavier.dupre@gmail.com>
2022-12-30 23:36:47 +0100

    Update conf.py

--- _doc/sphinxdoc/source/conf.py
+++ _doc/sphinxdoc/source/conf.py
@@ -9,7 +9,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.split(__file__)[0])))
 set_sphinx_variables(__file__, "pyensae", "Xavier Dupré", 2022,
                      "alabaster", alabaster.get_path(),
                      locals(), add_extensions=None,
-                     extlinks=dict(issue=('https://github.com/sdpython/pyensae/issues/%s', 'issue')))
+                     extlinks=dict(issue=(
+                         'https://github.com/sdpython/pyensae/issues/%s',
+                         'issue %s')))
 
 blog_root = "http://www.xavierdupre.fr/app/pyensae/helpsphinx/"
 blog_background = False
